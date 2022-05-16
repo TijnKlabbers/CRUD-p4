e3497b215fdf504304a6766b1ba1d910ed11a37c
kelvin0307 <kelvinlvb2002@hotmail.com>
2022-05-16 15:48:49 +0200

    php Login

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,10 @@
 <header>
+  <?php
+    include_once "connect.php";
+  ?>
     <nav>
       <a href="index.php">HOME</a>
-      <a href="">OVER ONS</a>
+      <a href="login.php">LOGIN</a>
       <a href="reizen.php">LOCATIONS</a>
       <a href="#about">CONTACT</a>
     </nav>

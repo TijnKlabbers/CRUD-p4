26cef8de40db2407fba9c7e24ce256b7bcf0f4d2
kelvin0307 <kelvinlvb2002@hotmail.com>
2022-06-01 14:20:21 +0200

    linking

--- reizen.php
+++ reizen.php
@@ -20,28 +20,7 @@
 <body>
     <!-- header section starts  -->
 
-    <header>
-        <div id="menu-bar" class="fas fa-bars"></div>
-
-        <a href="#" class="logo"><span>T</span>ravel</a>
-
-        <nav class="navbar">
-            <a href="index.php">home</a>
-            <a href="reizen.php">locations</a>
-            <a href="overOns.php">about us</a>
-            <a href="contact.php">contact</a>
-        </nav>
-
-        <div class="icons">
-            <i class="fas fa-search" id="search-btn"></i>
-            <i class="fas fa-user" id="login-btn"></i>
-        </div>
-
-        <form action="" class="search-bar-container">
-            <input type="search" id="search-bar" placeholder="search here..." />
-            <label for="search-bar" class="fas fa-search"></label>
-        </form>
-    </header>
+    <?php include_once "includes/header.php" ?>
 
     <!-- header section ends -->
 

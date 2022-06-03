09ec9ecc81d9b383e88feb22d798a13addaf0477
TijnKlabbers <94520586+TijnKlabbers@users.noreply.github.com>
2022-06-03 10:56:32 +0200

    nieuwe code (#11)

--- includes/header.php
+++ includes/header.php
@@ -1,29 +1,23 @@
-<header>
-<?php include_once "includes/connect.php";?>
-      <div id="menu-bar" class="fas fa-bars"></div>
+<header> 
+  <?php include_once "includes/connect.php";?>
+  <div id="menu-bar" class="fas fa-bars"></div>
 
+  <a href="#" class="logo"><span>T</span>errific <span>T</span>ravels</a>
 
-    <nav class="navbar">
-      <a href="index.php">home</a>
-      <a href="#over ons">about us</a>
-      <a href="reizen.php">locations</a>
-      <a href="#contact">contact</a>
+  <nav class="navbar">
+    <a href="index.php">HOME</a>
+    <a href="reizen.php">LOCATIONS</a>
+    <a href="overOns.php">ABOUT US</a>
+    <a href="contact.php">CONTACT</a>
+  </nav>
 
+  <div class="icons">
+    <i class="fas fa-search" id="search-btn"></i>
+    <i class="fas fa-user" id="login-btn"></i>
+  </div>
 
-      <nav class="navbar">
-          <a href="index.php">home</a>
-          <a href="reizen.php">locations</a>
-          <a href="overOns.php">about us</a>
-          <a href="contact.php">contact</a>
-      </nav>
-
-      <div class="icons">
-          <i class="fas fa-search" id="search-btn"></i>
-          <i class="fas fa-user" id="login-btn"></i>
-      </div>
-
-      <form action="" class="search-bar-container">
-          <input type="search" id="search-bar" placeholder="search here..." />
-          <label for="search-bar" class="fas fa-search"></label>
-      </form>
-  </header>
+  <form action="" class="search-bar-container">
+    <input type="search" id="search-bar" placeholder="search here..." />
+    <label for="search-bar" class="fas fa-search"></label>
+  </form>
+</header>

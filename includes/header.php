3dfa647fbd82c944b1761f4a44666412a5ea4a7a
TijnKlabbers <1195159@student.roc-nijmegen.nl>
2022-06-03 10:25:10 +0200

    nieuwe code

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,23 @@
-<header>
+<header> 
   <?php include_once "includes/connect.php";?>
-    <div id="menu-bar" class="fas fa-bars"></div>
-    <a href="#" class="logo"><span>T</span>ravel</a>
+  <div id="menu-bar" class="fas fa-bars"></div>
 
-    <nav class="navbar">
-      <a href="index.php">home</a>
-      <a href="#over ons">about us</a>
-      <a href="#locaties">locations</a>
-      <a href="#contact">contact</a>
+  <a href="#" class="logo"><span>T</span>errific <span>T</span>ravels</a>
 
-    </nav>
+  <nav class="navbar">
+    <a href="index.php">HOME</a>
+    <a href="reizen.php">LOCATIONS</a>
+    <a href="overOns.php">ABOUT US</a>
+    <a href="contact.php">CONTACT</a>
+  </nav>
 
-    <div class="icons">
-      <i class="fas fa-user" id="login-btn"></i>
-    </div>
+  <div class="icons">
+    <i class="fas fa-search" id="search-btn"></i>
+    <i class="fas fa-user" id="login-btn"></i>
+  </div>
 
-  </header>
+  <form action="" class="search-bar-container">
+    <input type="search" id="search-bar" placeholder="search here..." />
+    <label for="search-bar" class="fas fa-search"></label>
+  </form>
+</header>
--- index.php
+++ index.php
@@ -25,28 +25,7 @@
   <body>
     <!-- header section starts  -->
 
-    <header>
-      <div id="menu-bar" class="fas fa-bars"></div>
-
-      <a href="#" class="logo"><span>T</span>errific <span>T</span>ravels</a>
-
-      <nav class="navbar">
-        <a href="index.php">HOME</a>
-        <a href="reizen.php">LOCATIONS</a>
-        <a href="overOns.php">ABOUT US</a>
-        <a href="contact.php">CONTACT</a>
-      </nav>
-
-      <div class="icons">
-        <i class="fas fa-search" id="search-btn"></i>
-        <i class="fas fa-user" id="login-btn"></i>
-      </div>
-
-      <form action="" class="search-bar-container">
-        <input type="search" id="search-bar" placeholder="search here..." />
-        <label for="search-bar" class="fas fa-search"></label>
-      </form>
-    </header>
+    <?php include_once "includes/header.php" ?>
 
     <!-- header section ends -->
 

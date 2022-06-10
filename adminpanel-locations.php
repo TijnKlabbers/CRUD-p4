782521c7b1b40bebcdec7aed566771bbcab848f1
TijnKlabbers <94520586+TijnKlabbers@users.noreply.github.com>
2022-06-10 10:34:01 +0200

    code betreft adminpanel (#13)

--- adminpanel-locations.php
+++ adminpanel-locations.php
@@ -8,7 +8,7 @@
     <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="css/adminPanel.css" />
+     <link rel="stylesheet" href="css/adminPanel-locations.css" />
    </head>
 <body>
   <div class="sidebar">
@@ -29,14 +29,13 @@
           </a>
         </li>
         <li>
-        <a href="adminpanel-userAccounts.php">
-
+          <a href="adminpanel-userAccounts.php">
             <i class='bx bx-list-ul' ></i>
             <span class="links_name">user accounts</span>
           </a>
         </li>
         <li>
-          <a href="adminpanel/adminpanel-bookedTravels.php">
+        <a href="adminpanel-bookedTravels.php">
             <i class='bx bx-pie-chart-alt-2' ></i>
             <span class="links_name">Bookings</span>
           </a>
@@ -62,39 +61,7 @@
 
     <div class="home-content">
       <div class="overview-boxes">
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total travels</div>
-            <div class="number">9</div>
-            <div class="indicator">
-            </div>
-          </div>
         </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total accounts</div>
-            <div class="number">0</div>
-            <div class="indicator">
-            </div>
-          </div>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total messages</div>
-            <div class="number">0</div>
-            <div class="indicator">
-            </div>
-          </div>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total reviews </div>
-            <div class="number">0</div>
-            <div class="indicator">
-            </div>
-          </div>
-        </div>
-      </div>
 
       <div class="sales-boxes">
         <div class="recent-sales box">
@@ -139,71 +106,7 @@
              <li><a href="#">$46.52</a></li>
           </ul>
           </div>
-          <div class="button">
-            <a href="#">See All</a>
-          </div>
         </div>
-        <div class="top-sales box">
-          <div class="title">Recent bookings</div>
-          <ul class="top-sales-details">
-            <li>
-            <a href="#">
-              <!--<img src="images/sunglasses.jpg" alt="">-->
-              <span class="product">Vuitton Sunglasses</span>
-            </a>
-            <span class="price">$1107</span>
-          </li>
-          <li>
-            <a href="#">
-               <!--<img src="images/jeans.jpg" alt="">-->
-              <span class="product">Hourglass Jeans </span>
-            </a>
-            <span class="price">$1567</span>
-          </li>
-          <li>
-            <a href="#">
-             <!-- <img src="images/nike.jpg" alt="">-->
-              <span class="product">Nike Sport Shoe</span>
-            </a>
-            <span class="price">$1234</span>
-          </li>
-          <li>
-            <a href="#">
-              <!--<img src="images/scarves.jpg" alt="">-->
-              <span class="product">Hermes Silk Scarves.</span>
-            </a>
-            <span class="price">$2312</span>
-          </li>
-          <li>
-            <a href="#">
-              <!--<img src="images/blueBag.jpg" alt="">-->
-              <span class="product">Succi Ladies Bag</span>
-            </a>
-            <span class="price">$1456</span>
-          </li>
-          <li>
-            <a href="#">
-              <!--<img src="images/bag.jpg" alt="">-->
-              <span class="product">Gucci Womens's Bags</span>
-            </a>
-            <span class="price">$2345</span>
-          <li>
-            <a href="#">
-              <!--<img src="images/addidas.jpg" alt="">-->
-              <span class="product">Addidas Running Shoe</span>
-            </a>
-            <span class="price">$2345</span>
-          </li>
-<li>
-            <a href="#">
-             <!--<img src="images/shirt.jpg" alt="">-->
-              <span class="product">Bilack Wear's Shirt</span>
-            </a>
-            <span class="price">$1245</span>
-          </li>
-          </ul>
-        </div>
-      </div>
     </div>
   </section>
 
